c8f2096919f28182524a1eafbb9ba527321c8027
djtfmartin <djtfmartin@gmail.com>
2013-06-18 04:27:47 +0000

    0.1.10
    git-svn-id: http://ala.googlecode.com/svn/trunk/ala-web-theme@125 cf76a52e-83f7-1f8e-e3a8-b8364d34af1a

--- AlaWebThemeGrailsPlugin.groovy
+++ AlaWebThemeGrailsPlugin.groovy
@@ -3,7 +3,7 @@ import grails.util.Holders
 
 class AlaWebThemeGrailsPlugin {
     // the plugin version
-    def version = "0.1.8"
+    def version = "0.1.10"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.1 > *"
     // the other plugins this plugin depends on
