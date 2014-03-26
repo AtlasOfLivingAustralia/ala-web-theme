07f98f4c185d644d0e14e231445fbfc590c0c137
djtfmartin <djtfmartin@gmail.com>
2014-03-26 05:36:45 +0000

    Tagging the 0.2.4 version of the plugin source.
    git-svn-id: http://ala.googlecode.com/svn/trunk/ala-web-theme@196 cf76a52e-83f7-1f8e-e3a8-b8364d34af1a

---
+++ tags/v0_2_4/test/unit/ala/web/theme/AuthServiceTests.groovy
@@ -0,0 +1,17 @@
+package ala.web.theme
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(AuthService)
+class AuthServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
