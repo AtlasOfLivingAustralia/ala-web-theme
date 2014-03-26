024fd541536779453cc84d0c484cc469342f0905
djtfmartin <djtfmartin@gmail.com>
2014-03-26 05:38:03 +0000

    Tagging the 0.2.4 version of the plugin source.
    git-svn-id: http://ala.googlecode.com/svn/trunk/ala-web-theme@198 cf76a52e-83f7-1f8e-e3a8-b8364d34af1a

---
+++ tags/v0_2_4/grails-app/services/au/org/ala/web/AuthService.groovy
@@ -0,0 +1,141 @@
+package au.org.ala.web
+
+import au.org.ala.cas.util.AuthenticationCookieUtils
+import org.apache.commons.lang.StringUtils
+import net.sf.json.JSONArray
+import net.sf.json.JSONObject
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.web.context.request.RequestContextHolder
+
+class AuthService {
+    static transactional = false
+
+    def httpWebService, grailsApplication
+
+    def getEmail() {
+        def email = RequestContextHolder.currentRequestAttributes()?.getUserPrincipal()?.attributes?.email
+
+        if (!email) {
+            email  = AuthenticationCookieUtils.getUserName(RequestContextHolder.currentRequestAttributes().getRequest())
+        }
+
+        email
+    }
+
+    def getUserId() {
+        def userId = RequestContextHolder.currentRequestAttributes()?.getUserPrincipal()?.attributes?.userid
+
+        if (!userId) {
+            userId = AuthenticationCookieUtils.getUserName(RequestContextHolder.currentRequestAttributes().getRequest())
+        }
+
+        userId
+    }
+
+    def getDisplayName() {
+        if(RequestContextHolder.currentRequestAttributes()?.getUserPrincipal()?.attributes?.firstname){
+            ((RequestContextHolder.currentRequestAttributes()?.getUserPrincipal()?.attributes?.firstname) +
+                    " " + (RequestContextHolder.currentRequestAttributes()?.getUserPrincipal()?.attributes?.lastname))
+        } else {
+            null
+        }
+    }
+
+    public boolean userInRole(role) {
+        log.debug("userInRole: " + RequestContextHolder.currentRequestAttributes()?.isUserInRole(role))
+        return grailsApplication.config.security.cas.bypass ||
+                RequestContextHolder.currentRequestAttributes()?.isUserInRole(role) // || isAdmin()
+    }
+
+    def userDetails() {
+        def attr = RequestContextHolder.currentRequestAttributes()?.getUserPrincipal()?.attributes
+        def details = null
+
+        if (attr) {
+            details = [
+                    userId:attr?.userid?.toString(),
+                    email: attr?.email?.toString()?.toLowerCase(),
+                    userDisplayName: "${attr?.firstname?:""} ${attr?.lastname?:""}".trim()
+            ]
+        }
+
+        details
+    }
+
+    UserDetails getUserForUserId(String userId) {
+        // NOTE: the following method call will NOT be cached as caching only works from external classes
+        Map<String, UserDetails> um = getAllUserNameMap()
+        UserDetails ud = null;
+
+        if (um && um.containsKey(userId)) {
+            ud = um.get(userId)
+        }
+
+        return ud
+    }
+
+    @Cacheable("userMapCache")
+    Map<String, UserDetails> getAllUserNameMap() {
+        def userListMap = [:]
+        checkConfig()
+        try {
+            def userListJson = httpWebService.doJsonPost(grailsApplication.config.userDetails.url, grailsApplication.config.userDetails.path, "", "")
+
+            if (userListJson instanceof JSONObject) {
+                // works with path = getUserListWithIds
+                userListJson.keySet().each { id ->
+                    userListMap.put(String.valueOf(id), userListJson[id]);
+                }
+            } else if (userListJson instanceof JSONArray) {
+                // works with path = getUserListFull
+                userListJson.eachWithIndex { user, i ->
+                    def userDetails = new UserDetails(userId: user.id, displayName: "${user.firstName} ${user.lastName}", userName: user.userName)
+                    userListMap.put(String.valueOf(user.id), userDetails);
+                }
+            } else {
+                log.info "error -  " + userListJson.getClass() + ": ${userListJson}"
+            }
+        } catch (Exception e) {
+            log.error "Cache refresh error: " + e.message, e
+        }
+
+        return userListMap
+    }
+
+    @Cacheable("userListCache")
+    def getAllUserNameList() {
+        def userList = []
+        checkConfig()
+        try {
+            def userListJson = httpWebService.doJsonPost(grailsApplication.config.userDetails.url, grailsApplication.config.userDetails.path, "", "")
+
+            if (userListJson instanceof JSONObject) {
+                // works with path = getUserListWithIds
+                userListJson.keySet().each { id ->
+                    userList.add(userListJson[id]);
+                }
+            } else if (userListJson instanceof JSONArray) {
+                // works with path = getUserListFull
+                userListJson.eachWithIndex { user, i ->
+                    def userDetails = new UserDetails(userId: user.id, displayName: "${user.firstName} ${user.lastName}", userName: user.userName)
+                    userList.add(userDetails);
+                }
+            } else {
+                log.info "error -  " + userListJson.getClass() + ": ${userListJson}"
+            }
+        } catch (Exception e) {
+            log.error "Cache refresh error: " + e.message, e
+        }
+
+        return userList
+    }
+
+    def checkConfig() {
+        if (!grailsApplication.config.userDetails.url) {
+            log.error "Required config not found: userDetails.url - please add to Config.groovy"
+        }
+        if (!grailsApplication.config.userDetails.path) {
+            log.error "Required config not found: userDetails.path - please add to Config.groovy"
+        }
+    }
+}
