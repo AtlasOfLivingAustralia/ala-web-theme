9113db84cdf498343d99e42758e64689f540661a
davidbairdala <david.baird.ala@gmail.com>
2014-04-01 03:04:56 +0000

    Made CASRoles constants final
    git-svn-id: http://ala.googlecode.com/svn/trunk/ala-web-theme@217 cf76a52e-83f7-1f8e-e3a8-b8364d34af1a

--- src/groovy/au/org/ala/web/CASRoles.groovy
+++ src/groovy/au/org/ala/web/CASRoles.groovy
@@ -1,11 +1,8 @@
 package au.org.ala.web
 
-/**
- * Created by bai159 on 18/03/2014.
- */
 public class CASRoles {
 
-    public static String ROLE_ADMIN = "ROLE_ADMIN"
-    public static String ROLE_USER = "ROLE_USER"
+    public static final String ROLE_ADMIN = "ROLE_ADMIN"
+    public static final String ROLE_USER = "ROLE_USER"
 
 }
