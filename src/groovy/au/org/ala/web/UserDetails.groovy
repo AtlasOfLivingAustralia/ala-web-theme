46c18df25f7149ec44cbba84c8d31b6be0f02b3a
Simon Bear <simon.bear@csiro.au>
2014-12-22 18:28:53 +1100

    Make UserDetails serializable to prevent NotSerializableExceptions in logs when using ehcache with the userdetails web services with @Cacheable annotations

--- src/groovy/au/org/ala/web/UserDetails.groovy
+++ src/groovy/au/org/ala/web/UserDetails.groovy
@@ -1,11 +1,16 @@
 package au.org.ala.web
 
+import java.io.Serializable
+
 /**
  * Created with IntelliJ IDEA.
  *
  * @author "Nick dos Remedios <Nick.dosRemedios@csiro.au>"
  */
-class UserDetails {
+class UserDetails implements Serializable {
+
+    private static final long serialVersionUID = 42L;
+
     String displayName // full name
     String userName    // email
     String userId      // numeric id
